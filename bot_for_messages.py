d9d0bdea1e22bbbb1167cc4eb155739e34f945e1
Zerteg <fendrickov2017@yandex.ru>
2022-09-06 20:59:25 +0300

    wake_up

--- bot_for_messages.py
+++ bot_for_messages.py
@@ -1,3 +1,6 @@
+import os
+
+import dotenv
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater,
                           Filters,
@@ -5,12 +8,31 @@ from telegram.ext import (Updater,
                           CommandHandler)
 
 
-token = ""
+dotenv.load_dotenv()
+
+token = os.getenv("TOKEN")
 updater = Updater(token=token)
 
 
 def wake_up(update, context):
-    pass
+    chat = update.effective_chat
+    # button=ReplyKeyboardMarkup([['']])
+    if chat.last_name is None:
+        context.bot.send_message(
+           chat_id=chat.id,
+           text = 'Привет,  {}'.format(chat.firtst_name),
+        #    reply_markup=button 
+        )
+    else:
+        context.bot.send_message(
+            chat_id=chat.id,
+            text="Привет,  {} {}".format(chat.first_name, chat.last_name),
+            # reply_markup=button
+        )
+    context.bot.send_message(
+        chat_id = chat.id,
+        text = "Напишите id пользователя, которому надо отправить сообщение:"
+    )
 
 
 def get_id(update, context):
@@ -18,8 +40,12 @@ def get_id(update, context):
 
 
 def main():
-    pass
+    updater.dispatcher.add_handler(CommandHandler("start",wake_up))
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == "__main__":
     main()
+
+
