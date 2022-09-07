195089ef5676ebf1221b15519ac4bcbd48986d4d
Fizik05 <bboybaga13@mail.ru>
2022-09-07 23:36:40 +0300

    Our bot is ready

--- bot_for_messages.py
+++ bot_for_messages.py
@@ -2,10 +2,9 @@ import os
 
 import dotenv
 
-from telegram import ReplyKeyboardMarkup
 from telegram.ext import (Updater,
                           Filters,
-                          MessageFilter,
+                          MessageHandler,
                           CommandHandler)
 
 
@@ -13,24 +12,30 @@ dotenv.load_dotenv()
 
 token = os.getenv("TOKEN")
 updater = Updater(token=token)
-user_id = ""
-user_message = ""
+from_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+user_id = []
+user_message = []
+
+
+def make_file():
+    file = open("details.txt", "w")
+    file.write(user_id[0] + "\n")
+    file.write(user_message[0])
+    file.close()
 
 
 def wake_up(update, context):
     chat = update.effective_chat
-    # button=ReplyKeyboardMarkup([['']])
     if chat.last_name is None:
         context.bot.send_message(
            chat_id=chat.id,
-           text = 'Привет,  {}'.format(chat.firtst_name),
-        #    reply_markup=button 
+           text = 'Привет,  {}'.format(chat.firtst_name)
         )
     else:
         context.bot.send_message(
             chat_id=chat.id,
-            text="Привет,  {} {}".format(chat.first_name, chat.last_name),
-            # reply_markup=button
+            text="Привет,  {} {}".format(chat.first_name, chat.last_name)
         )
     context.bot.send_message(
         chat_id = chat.id,
@@ -38,32 +43,31 @@ def wake_up(update, context):
     )
 
 
-def get_message(message, id, context):
-    user_message = message
-
-    context.bot.send_message(
-        id,
-        text="Ваше сообщение отправлено"
-    )
-
-
 def get_id(update, context):
     message = update.message.text
     id = update.effective_chat.id
-    if user_id == "":
-        user_id = message
+    if user_id == []:
+        user_id.append(message)
 
         context.bot.send_message(
             id,
             text="Напишите мне сообщение, которое я должен отправить"
         )
     else:
-        get_message(message, id, context)
+        user_message.append(message)
+        make_file()
+        os.system("send_message.py")
+
+        context.bot.send_message(
+            id,
+            text="Ваше сообщение отправлено"
+        )
+        os.remove("details.txt")
 
 
 def main():
     updater.dispatcher.add_handler(CommandHandler("start", wake_up))
-    updater.dispatcher.add_handler(Filters.text, get_id)
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, get_id))
 
     updater.start_polling()
     updater.idle()
@@ -71,5 +75,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
